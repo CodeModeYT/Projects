e027539eb4916682b0123ee156ceda759550a20d
CodeMode <99124573+CodeModeYT@users.noreply.github.com>
2023-02-03 20:45:49 +0100

    Update createfiles.py
    Added first version of user control (not final, early upload because of IDE crash)

--- createfiles.py
+++ createfiles.py
@@ -1,12 +1,19 @@
 import os
 import glob
 
+amount = 10
+name = "File"
+filetype = "txt"
+
 def program():
-    for counter in range(10):
-        fp = open(f'Testfile{counter}.txt', 'w')
+    for counter in range(amount):
+        fp = open(f'{name}{counter}.{filetype}', 'w')
         fp.write(f'File number {counter}')
         fp.close()
         print(f"File number {counter} created")
         counter+=1
 
+print("Welcome to the mass file creator.")
+name=input("What name should the files have?")
+filetype=input("What should the type of file be? (soon, currently only supporting txt)")
 program()
