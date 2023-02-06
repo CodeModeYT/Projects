4c7f8d94049cdb03638f26b9c428819be8a1a560
CodeMode <99124573+CodeModeYT@users.noreply.github.com>
2023-02-06 20:15:09 +0100

    Update deletefile.py
    added advanced user control

--- deletefile.py
+++ deletefile.py
@@ -2,27 +2,27 @@ import os
 import glob
 import sys
 
-def count_txt_files(dir_path):
-    return len([f for f in os.listdir(dir_path) if f.endswith('.txt')])
-
-def program():
-    files = (glob.glob("D:/Path/to/file/*.txt"))
-    for file in files:
-        print(f"Removed {file}")
-        os.remove(file)
-
 def start():
-    dir_path = "D:/Path/to/file/"
-    txt_file_count = count_txt_files(dir_path)
-    check=input(f"Are you sure you want to continue and delete {txt_file_count} files? (y/n)")
+    filetype = input("Please input the filetype (without the dot): ")
+    dir_path = "D:Path/to/file/"
+    file_count = count_txt_files(dir_path, filetype)
+    check=input(f"Are you sure you want to continue and delete {file_count} files? (y/n)")
     if check == "y":
-        program()
+        files = (glob.glob(f"D:Path/to/file/*.{filetype}"))
+        for file in files:
+            os.remove(file)
+            print(f"Removed {file}")
     elif check == "n":
         print("Exiting...")
         sys.exit()
     else:
         print("This is not a valid option!")
         start()
+
+def count_txt_files(dir_path, filetype):
+    return len([f for f in os.listdir(dir_path) if f.endswith(f'.{filetype}')])
+
+
 
 print("Welcome to the mass file deleter.")
 start()
