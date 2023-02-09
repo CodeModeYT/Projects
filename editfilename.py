a937530b12a341ba9ff2435eb83f9caca93826db
CodeMode <99124573+CodeModeYT@users.noreply.github.com>
2023-02-09 21:10:04 +0100

    Update editfilename.py
    Added the libraries we're gonna use

--- editfilename.py
+++ editfilename.py
@@ -1,11 +1,11 @@
 import os
 import glob
-
+import sys
 
 
 def program():
     counter=1
-    files = (glob.glob("D:/path/to/file/*.txt"))
+    files = (glob.glob("D:/Path/to/file/*.txt"))
     for file in files:
         print(f"Renamed {file}")
         os.rename(file, f"Textfile-{counter}.txt")
